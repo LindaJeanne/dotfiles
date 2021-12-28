5c8500f2230008d82b16527272d5ddfd40335731
LindaJeanne <lindajeanne73@gmail.com>
2021-12-28 16:27:57 -0500

    fixes

--- symlinks.sh
+++ symlinks.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+mkdir .config/i3
+mkdir .config/i3status
+mkdir .config/terminator
+mkdir .config/rclone
+
 exec ln -sf ~/dotfiles/bashrc ~/.bashrc
 exec ln -sf ~/dotfiles/gitconfig ~/.gitconfig
 exec ln -sf ~/dotfiles/gitignore ~/.gitignore
-exec ln -sf ~/dotfiles/i3conf ~/.config/i3/conf
+exec ln -sf ~/dotfiles/i3conf ~/.config/i3/config
+exec ln -sf ~/dotfiles/i3status ~/.config/i3/conf
+
 exec ln -sf ~/dotfiles/vimrc ~/.vimrc
 exec ln -sf ~/dotfiles/xinitrc ~/.xinitrc
-exec ln -sf ~/dotfiles/xmodmaprc ~/.xmodmaprc
+# exec ln -sf ~/dotfiles/xmodmaprc ~/.xmodmaprc
 exec ln -sf ~/dotfiles/redshift.conf ~/.config/redshift.conf
-exec ln -sf ~/dotfiles/terminator ~/.config/terminator/conf
+exec ln -sf ~/dotfiles/terminator ~/.config/terminator/config
 exec ln -sf ~/dotfiles/rclone ~/.config/rclone/rclone.conf
 
-# the path to i3status is given in the i3conf config file, no link needed.
+# Fonts: Fantasque, Monofur, Terminus, Hermit, Yana
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
